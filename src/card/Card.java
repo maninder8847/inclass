1505b6378e0542f3283fe3c36e929db9daa43695
manin <manin@10.48.19.148>
2024-01-31 16:58:03 -0500

    added code

--- src/card/Card.java
+++ src/card/Card.java
@@ -1,61 +1,57 @@
-package card;
+package exercise1;
 
-import java.util.Random;
-import java.util.Scanner;
+/**
+ * A class that models playing card Objects. Cards have 
+ * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
+ * A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers.
+ * This code is to be used in ICE1. When you create your own branch,
+ * add your name as a modifier.
+ * @author dancye
+ * @author Paul Bonenfant May 2020
+ */
+public class Card {
 
-public class CardTrick {
-    
-    public static void main(String[] args) {
-        Card[] magicHand = new Card[7];
-        Random random = new Random();
-        
-        // Fill the magic hand with random cards
-        for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            c.setValue(random.nextInt(13) + 1); // Values from 1 to 13
-            c.setSuit(Card.SUITS[random.nextInt(Card.SUITS.length)]); // Suits from the SUITS array
-            magicHand[i] = c; // Add card to magic hand
-        }
-        
-        // Hard-coded lucky card
-        Card luckyCard = new Card();
-        luckyCard.setValue(2); // Value is 2
-        luckyCard.setSuit("Clubs"); // Suit is clubs
-        
-        // Ask the user for a card value and suit
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a card value (1-13 where Ace=1, Jack=11, Queen=12, King=13):");
-        int value = scanner.nextInt();
-        System.out.println("Enter a suit (Hearts, Diamonds, Spades, Clubs):");
-        String suit = scanner.next();
-        
-        // Create the user's card
-        Card userCard = new Card();
-        userCard.setValue(value);
-        userCard.setSuit(suit);
-        
-        // Search for the user's card in the magic hand
-        boolean found = false;
-        for (Card card : magicHand) {
-            if (card.getValue() == userCard.getValue() && card.getSuit().equalsIgnoreCase(userCard.getSuit())) {
-                found = true;
-                break;
-            }
-        }
-        
-        // Report the result
-        if (found) {
-            System.out.println("Your card is in the magic hand!");
-        } else {
-            System.out.println("Your card is not in the magic hand.");
-        }
-        
-        // Check if the user's card matches the lucky card
-        if (userCard.getValue() == luckyCard.getValue() && userCard.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
-            System.out.println("You picked the lucky card!");
-        }
-        
-        // Close the scanner
-        scanner.close();
+   private String suit; //clubs, spades, diamonds, hearts
+   private int value;//1-13
+   
+       public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
     }
+
+   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
+    Card() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }  
 }
